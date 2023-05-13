e694dcd0078cf1672404870fe37c7a4025e1ba0b
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2023-05-12 20:43:33 -0400

    project done

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Download from './Download';
+import Features from './Features';
+import SectionWrapper from './SectionWrapper';
+import Navbar from './Navbar';
+
+export { 
+  Download,
+  Features,
+  SectionWrapper,
+  Navbar
+}
